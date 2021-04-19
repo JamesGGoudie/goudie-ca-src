3956e68ee28e1eb2b53930cadda03003027d7236
James Gabriel Goudie <james.g.goudie@gmail.com>
2021-04-18 23:33:00 -0400

    Filled in skills

--- src/constants/skills-info.ts
+++ src/constants/skills-info.ts
@@ -11,6 +11,62 @@ export const SKILLS_INFO: SkillsGroupProps[] = [
 			{
 				name: 'JavaScript',
 				summary: 'Event Driver; Imperative'
+			},
+			{
+				name: 'TypeScript',
+				summary: 'Object Oriented; Imperative'
+			},
+			{
+				name: 'HTML',
+				summary: 'Web Document Formatting'
+			},
+			{
+				name: 'CSS',
+				summary: 'Web Document Styling'
+			},
+			{
+				name: 'SCSS',
+				summary: 'Web Document Styling'
+			},
+			{
+				name: 'C',
+				summary: 'Imperative; Structured'
+			},
+			{
+				name: 'C++',
+				summary: 'Object Oriented; Procedural'
+			},
+			{
+				name: 'Python',
+				summary: 'Object Oriented; Functional; Structured'
+			},
+			{
+				name: 'C#',
+				summary: 'Object Oriented; Structured; Imperative'
+			},
+			{
+				name: 'Kotlin',
+				summary: 'Object Oriented; Imperative'
+			},
+			{
+				name: 'Visual Basic',
+				summary: 'Event-Based; Object Oriented'
+			},
+			{
+				name: 'MatLab',
+				summary: 'Numerical Computing'
+			},
+			{
+				name: 'R',
+				summary: 'Statistical Computing'
+			},
+			{
+				name: 'Racket',
+				summary: 'Functional'
+			},
+			{
+				name: 'Haskell',
+				summary: 'Functional'
 			}
 		]
 	},
@@ -24,6 +80,241 @@ export const SKILLS_INFO: SkillsGroupProps[] = [
 			{
 				name: 'Angular',
 				summary: 'Web Framework'
+			},
+			{
+				name: 'React',
+				summary: 'UI Library'
+			},
+			{
+				name: 'Ext JS',
+				summary: 'Web Framework'
+			},
+			{
+				name: 'Node.js',
+				summary: 'Runtime Environment'
+			},
+			{
+				name: 'Apollo GraphQL',
+				summary: 'API Query and Manipulation'
+			},
+			{
+				name: 'Android',
+				summary: 'Mobile Applications'
+			},
+			{
+				name: 'Unity',
+				summary: 'Game Engine'
+			},
+			{
+				name: 'Unreal',
+				summary: 'Game Engine'
+			}
+		]
+	},
+	{
+		name: 'Project Management',
+		skills: [
+			{
+				name: 'GitLab',
+				summary: 'Git Server + CI/CD Pipelines'
+			},
+			{
+				name: 'GitHub',
+				summary: 'Git Server'
+			},
+			{
+				name: 'Jira',
+				summary: 'Agile Team Management'
+			},
+			{
+				name: 'Jenkins',
+				summary: 'Automation'
+			},
+			{
+				name: 'Crucible',
+				summary: 'Code Review'
+			}
+		]
+	},
+	{
+		name: 'Project Architecture',
+		skills: [
+			{
+				name: 'Ansible',
+				summary: 'Configuration Management'
+			},
+			{
+				name: 'Apache Kafka',
+				summary: 'Stream Processing'
+			},
+			{
+				name: 'FlyWay',
+				summary: 'Database Migration'
+			},
+			{
+				name: 'Apache Geode',
+				summary: 'In-Memory Database'
+			},
+			{
+				name: 'Apache Ant',
+				summary: 'Automate Software Build Processes'
+			},
+			{
+				name: 'Nginx',
+				summary: 'Web Server'
+			},
+			{
+				name: 'Amazon S3',
+				summary: 'Object Storage'
+			},
+			{
+				name: 'Keycloak',
+				summary: 'Identity and Access Management'
+			}
+		]
+	},
+	{
+		name: 'Databases',
+		skills: [
+			{
+				name: 'PostgreSQL',
+				summary: 'SQL Relational Database'
+			},
+			{
+				name: 'MySQL',
+				summary: 'SQL Relational Database'
+			},
+			{
+				name: 'SQLite',
+				summary: 'Embedded SQL Relational Database '
+			},
+			{
+				name: 'MongoDB',
+				summary: 'NoSQL Database '
+			},
+			{
+				name: 'NeDB',
+				summary: 'Embedded NoSQL Database'
+			}
+		]
+	},
+	{
+		name: 'Libraries',
+		skills: [
+			{
+				name: 'Angular Material',
+				summary: 'UI Components Library'
+			},
+			{
+				name: 'Ionic',
+				summary: 'UI Components Library'
+			},
+			{
+				name: 'Matplotlib',
+				summary: 'Python Plotting Library'
+			},
+			{
+				name: 'NumPy',
+				summary: 'Python Math Support'
+			},
+			{
+				name: 'Three.js',
+				summary: 'JavaScript 3D Graphics'
+			},
+			{
+				name: 'D3.js',
+				summary: 'JavaScript Data Visualization'
+			},
+			{
+				name: 'LLVM',
+				summary: 'C++ Compiler Infrastructure'
+			},
+			{
+				name: 'PDFBox',
+				summary: 'Java PDF Generation'
+			},
+			{
+				name: 'PDFlib',
+				summary: 'Java PDF Generation'
+			}
+		]
+	},
+	{
+		name: 'Development Tools',
+		skills: [
+			{
+				name: 'Postman',
+				summary: 'API Development'
+			},
+			{
+				name: 'Fiddler',
+				summary: 'Network Debugger'
+			},
+			{
+				name: 'SonarQube',
+				summary: 'Code Quality'
+			},
+			{
+				name: 'Veracode',
+				summary: 'Code Quality'
+			},
+			{
+				name: 'Valgrind',
+				summary: 'Memory Debugging'
+			},
+			{
+				name: 'Ghidra',
+				summary: 'Reverse Engineering'
+			}
+		]
+	},
+	{
+		name: 'IDEs',
+		skills: [
+			{
+				name: 'Android Studio',
+				summary: 'Android Development'
+			},
+			{
+				name: 'Eclipse',
+				summary: 'Java Development'
+			},
+			{
+				name: 'Spring Tools Suite',
+				summary: 'Spring Development'
+			},
+			{
+				name: 'IntelliJ IDEA',
+				summary: 'Java Development'
+			},
+			{
+				name: 'Visual Studio Code',
+				summary: 'JavaScript, TypeScript, HTML, and CSS Development'
+			},
+			{
+				name: 'Microsoft Visual Studio',
+				summary: 'C, C++, and C# Development'
+			},
+			{
+				name: 'Wing',
+				summary: 'Python Development'
+			}
+		]
+	},
+	{
+		name: 'Hardware',
+		skills: [
+			{
+				name: 'Verilog',
+				summary: 'Model Electronic Systems'
+			},
+			{
+				name: 'Arduino',
+				summary: 'Single-Board Micro-controllers'
+			},
+			{
+				name: 'NCX',
+				summary: 'Lego Mindstorms'
 			}
 		]
 	}
